63217a74074499a2e133d7d5c341afde872f0929
Alexander Vieth <alexander.vieth@mail.mcgill.ca>
2015-06-02 18:24:04 -0400

    Remove ifUnauthorized handlr frm withAuthorization
    Similar changes found in Authenticake.

--- Authoricecream/Authorize.hs
+++ Authoricecream/Authorize.hs
@@ -5,11 +5,14 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE StandaloneDeriving #-}
 
 module Authoricecream.Authorize (
 
     Authorized
   , withAuthorization
+  , AuthorizationOutcome(..)
 
   , authorizedResource
   , authorizedThing
@@ -40,6 +43,18 @@ authorizedThing = Authorized $ lift authenticatedThing
 authorizedContext :: (Functor m, Monad m) => Authorized ctx t r m ctx
 authorizedContext = Authorized $ lift authenticatedContext
 
+data AuthorizationOutcome ctx datum rsrc t where
+  AuthorizationFailed :: NotAuthorizedReason ctx datum rsrc -> AuthorizationOutcome ctx datum rsrc t
+  AuthorizationOK :: t -> AuthorizationOutcome ctx datum rsrc t
+
+deriving instance (Show t, Show (NotAuthorizedReason ctx datum rsrc))
+  => Show (AuthorizationOutcome ctx datum rsrc t)
+
+instance Functor (AuthorizationOutcome ctx datum rsrc) where
+  fmap f term = case term of
+      AuthorizationFailed x -> AuthorizationFailed x
+      AuthorizationOK x -> AuthorizationOK (f x)
+
 withAuthorization
   :: forall ctx t r m a .
      ( Functor m
@@ -49,16 +64,14 @@ withAuthorization
   => ctx
   -> (forall s . AuthorizeF ctx t r s -> m s)
   -> r
-  -> (NotAuthorizedReason ctx t r -> Authenticate ctx t m a)
-  -- ^ in case not authorized!
   -> Authorized ctx t r m a
-  -> Authenticate ctx t m a
-withAuthorization ctx lifter resrc ifUnauthorized term = do
+  -> Authenticate ctx t m (AuthorizationOutcome ctx t r a)
+withAuthorization ctx lifter resrc term = do
     datum <- authenticatedThing
     decision <- lift . lifter $ authorize ctx datum resrc
     case decision of
-      Just denial -> ifUnauthorized denial
-      Nothing -> runReaderT (runAuthorized term) resrc
+      Just denial -> return (AuthorizationFailed denial)
+      Nothing -> fmap AuthorizationOK (runReaderT (runAuthorized term) resrc)
 
 class Authorizes ctx datum resource where
   type NotAuthorizedReason ctx datum resource
